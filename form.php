538f840745397d7de4c5c66f84e6ad7111e7954e
kristiafiliposki <filiposki.kristian1112@gmail.com>
2023-12-19 16:32:26 +0100

    first commit

---
+++ form.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Crea la tua password </h1>
+    <p>Inserisci la giusta lunghezza</p>
+    <form action="password.php" >
+        <input type="text" name="lenght" placeholder="lunghezza password">
+        <button type="submit">Invia</button>
+    </form>
+</body>
+</html>
---
+++ password.php
@@ -0,0 +1,18 @@
+    <?php 
+        $password = $_GET["lenght"];
+        function GenPassword($lenght,$arrey){
+            $numero = rand(0,9);
+        }
+    ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div><?php echo $password ?></div>
+    <a href="form.php"></a>
+</body>
+</html>
